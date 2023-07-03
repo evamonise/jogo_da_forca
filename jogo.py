57108a571fc64fa4e46f9b635cdfd90b711db5ba
Aluno <aluno@naverecife.local>
2023-07-03 20:00:57 -0300

    arquivos iniciais

---
+++ jogo.py
@@ -0,0 +1,51 @@
+import palavra
+
+letras_usuario = []
+
+chances = 4
+
+ganhou = False
+
+while True:
+
+    # criar a nossa logica
+
+    for letra in palavra:
+
+        if letra.lower() in letras_usuario:
+
+            print(letra, end=" ")
+
+        else:
+
+            print("_", end=" ")
+
+    print(f"Você tem {chances} chances")
+
+    tentativa = input("Digite uma letra para adivinhar a palavra: ")
+
+    letras_usuario.append(tentativa.lower())
+
+    if tentativa.lower() not in palavra.lower():
+
+        chances -= 1
+
+    ganhou = True
+
+    for letra in palavra:
+
+        if letra.lower() not in letras_usuario:
+
+            ganhou = False
+
+    if chances == 0 or ganhou:
+
+        break
+
+if ganhou:
+
+    print(f"Parabéns, você ganhou. A palavra era: {palavra}")
+
+else:
+
+    print(f"Você perdeu! A palavra era: {palavra}")
